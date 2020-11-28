42dac65a1c068f58bade1d66f32141250f64c49d
VjacheslavZ <vjacheslav.zemlianiy@fluvius.co>
2020-11-28 21:03:05 +0200

    add CreateTaskDto, implement @Get @Post @Delete for tasks controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Body, Param } from '@nestjs/common';
 
 import { TasksService } from './tasks.service';
 import { TaskModel } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -10,5 +11,20 @@ export class TasksController {
   @Get()
   getAllRAsks(): TaskModel[] {
     return this.tasksService.getAllTasks();
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): TaskModel {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Post()
+  createTask(@Body() CreateTaskDto: CreateTaskDto): TaskModel {
+    return this.tasksService.createTask(CreateTaskDto);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    this.tasksService.deleteTask(id);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { v1 as uuid } from 'uuid';
 
-import { TaskModel } from './task.model';
+import { TaskModel, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -8,5 +10,26 @@ export class TasksService {
 
   getAllTasks(): TaskModel[] {
     return this.tasks;
+  }
+
+  getTaskById(id: string): TaskModel {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  createTask(CreateTaskDto: CreateTaskDto): TaskModel {
+    const { title, description } = CreateTaskDto;
+    const task: TaskModel = {
+      title,
+      description,
+      status: TaskStatus.OPEN,
+      id: uuid(),
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+
+  deleteTask(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 }
